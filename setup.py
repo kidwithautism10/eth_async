fee0ed36d1e0cc39e2ff7e7997c294b76a9cfb7a
kidwithautism10 <kidwithautism10@gmail.com>
2024-06-11 14:22:48 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='eth_async',
+    version='1.0',
+    description='A useful module',
+    author='13',
+    author_email='author@gmail.com',
+    packages=['eth_async'],  # same as name
+    install_requires=[],  # external packages as dependencies
+)
